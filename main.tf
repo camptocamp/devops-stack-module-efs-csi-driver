7d30130181c40e0175051b971d976b18419fc6be
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-02-24 14:05:38 +0100

    feat: use name prefix instead of full name

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ module "iam_assumable_role_efs" {
   version                    = "~> 5.0"
   create_role                = var.create_role
   number_of_role_policy_arns = 1
-  role_name                  = format("efs-csi-driver-%s", var.cluster_name)
+  role_name_prefix           = format("efs-csi-driver-%s-", var.cluster_name)
   provider_url               = replace(var.cluster_oidc_issuer_url, "https://", "")
   role_policy_arns           = [resource.aws_iam_policy.efs.arn]
 
