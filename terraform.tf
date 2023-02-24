caedc31982d8c7e976f80df49f2c804dcd78dee1
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-02-24 14:41:26 +0100

    fix: add versions to terraform providers

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,12 @@
 terraform {
   required_providers {
     argocd = {
-      source = "oboukili/argocd"
+      source  = "oboukili/argocd"
+      version = "~> 4"
     }
     utils = {
-      source = "cloudposse/utils"
+      source  = "cloudposse/utils"
+      version = "~> 1"
     }
   }
 }
